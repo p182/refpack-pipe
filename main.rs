0e8e4e6cb4f3e8178985dedb4b607d8ee33e1dc1
p182 <33848191+p182@users.noreply.github.com>
2025-07-08 23:24:30 +0300

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,20 @@
+use std::io::{self, Read, Write, Cursor};
+use refpack::{format::SimEA, CompressionOptions, compress};
+
+fn main() {
+    let mut input = Vec::new();
+    io::stdin().read_to_end(&mut input).expect("Failed to read stdin");
+
+    let mut input_cursor = Cursor::new(&input);    // &[u8] + Seek
+    let mut output_cursor = Cursor::new(Vec::new()); // Vec<u8> + Seek
+
+    compress::<SimEA>(
+        input.len(),
+        &mut input_cursor,
+        &mut output_cursor,
+        CompressionOptions::default()
+    ).expect("Compression failed");
+
+    let compressed = output_cursor.into_inner();
+    io::stdout().write_all(&compressed).expect("Failed to write to stdout");
+}
